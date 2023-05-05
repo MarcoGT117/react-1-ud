efd20e06eb6876f5426f4432b07b3eeb899a6d02
Marco Gonzalez <marco.g.gleztorres@gmail.com>
2023-05-05 11:19:54 -0600

    Componentes

--- primer-app/src/App.js
+++ primer-app/src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+// Importando el primer componente
+import HolaMundo, {AdiosMundo} from './components/HolaMundo';
 
 function App() {
   return (
@@ -7,16 +9,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        Renderizando el componente HolaMundo
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <HolaMundo/>
+        <AdiosMundo/>
       </header>
     </div>
   );
---
+++ primer-app/src/components/HolaMundo.js
@@ -0,0 +1,28 @@
+// Este es el primer componente creado para la aplicacion.
+// Una buena practica para su creacion es crear una carpeta llamada components donde se guardaran y nombrarlos a todos con la primer letra en mayuscula
+
+// Se empieza con un import de react
+import React from "react";
+
+// Lo siguiente es la funcion que renderiza el componente, por lo general esta funcion tiene el mismo nombre del componente
+
+function HolaMundo() {
+  return (
+    <div>
+      <h1>Hola mundo!</h1>
+      <h2>Marco Gonzalez</h2>
+    </div>
+  );
+}
+
+// Por ultimo exportamos el componente para que pueda interactuar con el resto de la aplicacion
+export default HolaMundo;
+
+// Como no se pueden tener dos exports en el mismo archivo a este subcomponente le pondremos el export de manera directa, sin el default
+export function AdiosMundo() {
+  return (
+    <div>
+      <h3>Adios</h3>
+    </div>
+  );
+}
